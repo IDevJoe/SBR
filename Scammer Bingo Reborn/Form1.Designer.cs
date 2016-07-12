e22eab8a6ea99f950a644fdbc050846199eeb40a
Joe L <joethehumandev@gmail.com>
2016-07-12 12:01:08 -0500

    Additions
    - Added menu bar
    - Added about dialog
    - Added button click events
    - Centered score

--- Scammer Bingo Reborn/Form1.Designer.cs
+++ Scammer Bingo Reborn/Form1.Designer.cs
@@ -43,12 +43,20 @@
             this.button10 = new System.Windows.Forms.Button();
             this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutSBRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gitHubProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contributorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(9, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(119, 13);
             this.label1.TabIndex = 0;
@@ -57,134 +65,205 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(180, 339);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(141, 312);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.Size = new System.Drawing.Size(93, 31);
             this.label2.TabIndex = 1;
             this.label2.Text = "Score:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(224, 339);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(240, 312);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.Size = new System.Drawing.Size(67, 31);
             this.label3.TabIndex = 2;
             this.label3.Text = "0/10";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 39);
+            this.button1.Location = new System.Drawing.Point(16, 55);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(109, 80);
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(128, 39);
+            this.button2.Location = new System.Drawing.Point(131, 55);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(109, 80);
             this.button2.TabIndex = 4;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(243, 39);
+            this.button3.Location = new System.Drawing.Point(247, 55);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(109, 80);
             this.button3.TabIndex = 5;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(358, 39);
+            this.button4.Location = new System.Drawing.Point(361, 55);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(109, 80);
             this.button4.TabIndex = 6;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(12, 125);
+            this.button5.Location = new System.Drawing.Point(16, 141);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(109, 80);
             this.button5.TabIndex = 7;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(127, 125);
+            this.button6.Location = new System.Drawing.Point(131, 141);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(109, 80);
             this.button6.TabIndex = 8;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(242, 125);
+            this.button7.Location = new System.Drawing.Point(246, 141);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(109, 80);
             this.button7.TabIndex = 9;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(357, 125);
+            this.button8.Location = new System.Drawing.Point(361, 141);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(109, 80);
             this.button8.TabIndex = 10;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(15, 211);
+            this.button9.Location = new System.Drawing.Point(16, 227);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(109, 80);
             this.button9.TabIndex = 11;
             this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(127, 211);
+            this.button10.Location = new System.Drawing.Point(131, 227);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(109, 80);
             this.button10.TabIndex = 12;
             this.button10.Text = "button10";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(242, 211);
+            this.button11.Location = new System.Drawing.Point(247, 227);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(109, 80);
             this.button11.TabIndex = 13;
             this.button11.Text = "button11";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
-            this.button12.Location = new System.Drawing.Point(357, 211);
+            this.button12.Location = new System.Drawing.Point(361, 227);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(109, 80);
             this.button12.TabIndex = 14;
             this.button12.Text = "button12";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Started by DevJoe, created by the community";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(482, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(39, 20);
+            this.toolStripMenuItem1.Text = "SBR";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutSBRToolStripMenuItem,
+            this.gitHubProjectToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // aboutSBRToolStripMenuItem
+            // 
+            this.aboutSBRToolStripMenuItem.Name = "aboutSBRToolStripMenuItem";
+            this.aboutSBRToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutSBRToolStripMenuItem.Text = "About SBR";
+            this.aboutSBRToolStripMenuItem.Click += new System.EventHandler(this.aboutSBRToolStripMenuItem_Click);
+            // 
+            // gitHubProjectToolStripMenuItem
+            // 
+            this.gitHubProjectToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contributorsToolStripMenuItem});
+            this.gitHubProjectToolStripMenuItem.Name = "gitHubProjectToolStripMenuItem";
+            this.gitHubProjectToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.gitHubProjectToolStripMenuItem.Text = "GitHub Project";
+            // 
+            // contributorsToolStripMenuItem
+            // 
+            this.contributorsToolStripMenuItem.Name = "contributorsToolStripMenuItem";
+            this.contributorsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.contributorsToolStripMenuItem.Text = "Contributors";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 405);
+            this.ClientSize = new System.Drawing.Size(482, 352);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.button11);
             this.Controls.Add(this.button10);
@@ -200,10 +279,17 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(498, 391);
+            this.MinimumSize = new System.Drawing.Size(498, 391);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Scammer Bingo Reborn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -226,6 +312,13 @@
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutSBRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gitHubProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contributorsToolStripMenuItem;
     }
 }
 
--- Scammer Bingo Reborn/Form1.cs
+++ Scammer Bingo Reborn/Form1.cs
@@ -16,5 +16,76 @@ namespace Scammer_Bingo_Reborn
         {
             InitializeComponent();
         }
+
+        private void aboutSBRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 about = new AboutBox1();
+            about.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
