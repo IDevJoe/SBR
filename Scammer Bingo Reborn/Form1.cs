e97c708f60fe63443cff105a05b1815459f59d5c
imDema <dema3112@gmail.com>
2016-07-13 23:41:39 +0200

    Buttons now created at runtime from 2D string array

--- Scammer Bingo Reborn/Form1.cs
+++ Scammer Bingo Reborn/Form1.cs
@@ -15,6 +15,20 @@ namespace Scammer_Bingo_Reborn
         //Joe is awesome :D
         private static int score = 0;
 
+        //
+        private string[,] buttonText = 
+            {
+                { "these","are","just" },
+                { "as1","a23123s","a345s" },
+                { "these","are354345","just" },
+                { "5345as","a354s","as" }
+            };
+
+        //Percentage of white space between buttons
+        private float whitespaceX = 0.1f, whitespaceY = 0.1f;
+        //Offset from the top of the GroupBox (needed or the first button will go on top of the GroupBox's text)
+        private int offsetY = 10;
+
         public Form1()
         {
             InitializeComponent();
@@ -28,99 +42,45 @@ namespace Scammer_Bingo_Reborn
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            button13.Focus();
+            buttonReset.Focus();
+
+            Button[,] btns = new Button[buttonText.GetLength(0),buttonText.GetLength(1)];
+
+
+            for (int i = 0; i < buttonText.GetLength(0); i++)
+            {
+                for (int j = 0; j < buttonText.GetLength(1); j++)
+                {
+                    btns[i,j] = new Button();
+                    btns[i,j].Name = "btn" + i + "." + j;
+                    btns[i,j].Text = buttonText[i,j];
+                }
+            }
+            ArrangeButtons(btns, groupBox_BingoBoard);
+
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void ArrangeButtons(Button[,] btns, GroupBox container)
         {
-            buttonClick(sender);
-        }
+            int nX = btns.GetLength(0), nY = btns.GetLength(1);
+            int maxX = container.Width, maxY = container.Height - offsetY;
+            for (int i = 0; i < nX; i++)
+            {
+                for (int j = 0; j <nY; j++)
+                {
+                    int posX = Convert.ToInt32(((float)maxX / nX) * (i + whitespaceX/2));
+                    int posY = Convert.ToInt32(((float)maxY / nY) * ( j + whitespaceY/2)) + offsetY;
+                    btns[i, j].Location = new Point(posX, posY);
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
+                    int width, heigth;
+                    width = Convert.ToInt32((float)maxX / nX * (1 - whitespaceX));
+                    heigth = Convert.ToInt32((float)maxY / nY * (1 -whitespaceY));
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
+                    btns[i, j].Size = new Size(width, heigth);
 
-        private void button8_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-
-        private void button12_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-
-        private void button10_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-
-        private void button11_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-        private void button14_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-        private void button15_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-        private void button16_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-        private void button17_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-        private void button18_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-        private void button19_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-        private void button20_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-        private void button21_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            buttonClick(sender);
+                    this.groupBox_BingoBoard.Controls.Add(btns[i, j]);
+                }
+            }
         }
 
         private void buttonClick(object sender)
@@ -129,35 +89,16 @@ namespace Scammer_Bingo_Reborn
             string newScore = score + "/20";
             label3.Text = newScore;
             ((Button)sender).Enabled = false;
-            button13.Focus();
+            buttonReset.Focus();
         }
 
-        private void button13_Click(object sender, EventArgs e)
+        private void buttonReset_Click(object sender, EventArgs e)
         {
-            button1.Enabled = true;
-            button2.Enabled = true;
-            button3.Enabled = true;
-            button4.Enabled = true;
-            button5.Enabled = true;
-            button6.Enabled = true;
-            button7.Enabled = true;
-            button8.Enabled = true;
-            button9.Enabled = true;
-            button10.Enabled = true;
-            button11.Enabled = true;
-            button12.Enabled = true;
-            button14.Enabled = true;
-            button15.Enabled = true;
-            button16.Enabled = true;
-            button17.Enabled = true;
-            button18.Enabled = true;
-            button19.Enabled = true;
-            button20.Enabled = true;
-            button21.Enabled = true;
+
 
             score = 0;
             label3.Text = "0/20";
-            button13.Focus();
+            buttonReset.Focus();
         }
 
         private void contributorsToolStripMenuItem_Click(object sender, EventArgs e)
