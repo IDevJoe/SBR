2d0ada8eba7d2ed890a3dd4190986d2394d824e6
Joe <joethehumandev@gmail.com>
2016-08-02 18:27:23 -0500

    Made changes to UpdateBackend

--- Scammer Bingo Reborn/Settings.Designer.cs
+++ Scammer Bingo Reborn/Settings.Designer.cs
@@ -72,7 +72,7 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(348, 19);
+            this.button1.Location = new System.Drawing.Point(339, 19);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 23);
             this.button1.TabIndex = 1;
