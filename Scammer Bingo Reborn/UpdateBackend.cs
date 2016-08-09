4dffe3925e7effc1efd97707ea7392eba56c3732
Joe <joethehumandev@gmail.com>
2016-08-09 10:03:48 -0500

    Final edits - Updated the update server URLs, Hexxium is primary, Github is secondary - Updated the icon

--- Scammer Bingo Reborn/UpdateBackend.cs
+++ Scammer Bingo Reborn/UpdateBackend.cs
@@ -13,52 +13,109 @@ namespace Scammer_Bingo_Reborn
     class UpdateBackend
     {
 
-        public static object[] updateAvail()
+        public static object[] updateAvail(int serv)
         {
-            HttpWebRequest r = WebRequest.CreateHttp("http://joethehuman.github.io/projectupdates/sbr/ver.txt");
-            r.UserAgent = "Scammer Bingo Auto-Update";
-            string ver;
-            using (WebResponse resp = r.GetResponse())
+            string ver = "";
+            string server = "";
+            if (serv == 0)
             {
-                using (Stream data = resp.GetResponseStream())
+                server = "Primary";
+                try
                 {
-                    using (StreamReader read = new StreamReader(data))
+                    HttpWebRequest r = WebRequest.CreateHttp("http://www.hexxiumcreations.com/community/ver.txt");
+                    r.UserAgent = "Scammer Bingo Auto-Update";
+                    using (WebResponse resp = r.GetResponse())
                     {
-                        ver = read.ReadToEnd();
+                        using (Stream data = resp.GetResponseStream())
+                        {
+                            using (StreamReader read = new StreamReader(data))
+                            {
+                                ver = read.ReadToEnd();
+                            }
+                        }
                     }
+                }
+                catch (Exception e)
+                {
+                    return updateAvail(1);
+                }
+            } else
+            {
+                server = "Backup";
+                try
+                {
+                    HttpWebRequest r = WebRequest.CreateHttp("http://joethehuman.github.io/projectupdates/sbr/ver.txt");
+                    r.UserAgent = "Scammer Bingo Auto-Update";
+                    using (WebResponse resp = r.GetResponse())
+                    {
+                        using (Stream data = resp.GetResponseStream())
+                        {
+                            using (StreamReader read = new StreamReader(data))
+                            {
+                                ver = read.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    return new object[] { (false), Settings.settings.cversion, Settings.settings.cversion };
                 }
             }
 
                 
-            return new object[]{ (ver != Settings.settings.cversion), ver, Settings.settings.cversion };
+            return new object[]{ (ver != Settings.settings.cversion), ver, Settings.settings.cversion, server, serv };
         }
 
         public static bool checkForUpdates(Form1 m)
         {
-            object[] response = UpdateBackend.updateAvail();
+            object[] response = UpdateBackend.updateAvail(0);
             bool av = (bool)response[0];
             string latest = (string)response[1];
             string current = (string)response[2];
             if (av)
             {
-                DialogResult res = MessageBox.Show(null, "An update is available!\nInstalled version: " + current + "\nLatest version: " + latest + "\n\nWould you like to install this update now?", "Update available", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                DialogResult res = MessageBox.Show(null, "An update is available!\nInstalled version: " + current + "\nLatest version: " + latest + "\n\nWould you like to install this update now?", "Update available ("+((string)response[3])+" server)", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
                 if(res == DialogResult.Yes)
                 {
-                    update();
+                    update((int)response[4]);
                     m.Dispose();
                 }
             }
             return av;
         }
 
-        public static void update()
+        public static void update(int serv)
         {
-            WebClient l = new WebClient();
-            l.DownloadFile("http://joethehuman.github.io/projectupdates/sbr/lastestVersion.txt", "latestVersion.zip");
-            
-            ZipFile.ExtractToDirectory("latestVersion.zip", "update");
-            File.Delete("update.zip");
-            System.Diagnostics.Process.Start("update\\setup.exe");
+            if (serv == 0)
+            {
+                try
+                {
+                    WebClient l = new WebClient();
+                    l.DownloadFile("http://www.hexxiumcreations.com/community/lastestVersion.txt", "latestVersion.zip");
+
+                    ZipFile.ExtractToDirectory("latestVersion.zip", "update");
+                    File.Delete("update.zip");
+                    System.Diagnostics.Process.Start("update\\setup.exe");
+                } catch(Exception e)
+                {
+                    update(1);
+                }
+            } else
+            {
+                try
+                {
+                    WebClient l = new WebClient();
+                    l.DownloadFile("http://joethehuman.github.io/projectupdates/sbr/lastestVersion.txt", "latestVersion.zip");
+
+                    ZipFile.ExtractToDirectory("latestVersion.zip", "update");
+                    File.Delete("update.zip");
+                    System.Diagnostics.Process.Start("update\\setup.exe");
+                }
+                catch (Exception e)
+                {
+                }
+            }
         }
 
     }
